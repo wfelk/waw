e7609d0e2f9fcb830e583a0ff38e0929030fe889
Wilhelm Felk <w.felk@me.com>
2026-03-28 21:35:15 +0100

    Add Russian and Polish translations

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["de", "en"],
+  locales: ["de", "en", "ru", "pl"],
   defaultLocale: "de",
 })
