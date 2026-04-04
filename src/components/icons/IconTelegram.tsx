7445357e85bfe49ba7118900fcc34dc3807ba6d5
Wilhelm Felk <w.felk@me.com>
2026-04-05 00:37:37 +0200

    refactor(contact): extract messenger icons

---
+++ src/components/icons/IconTelegram.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  size?: number
+}
+
+export const IconTelegram = ({ size = 24 }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M9.417 15.181l-.397 5.584c.568 0 .814-.244 1.109-.537l2.663-2.545 5.518 4.041c1.012.564 1.725.267 1.998-.931L23.93 3.821l.001-.001c.321-1.496-.541-2.081-1.527-1.714l-21.29 8.151c-1.453.564-1.431 1.374-.247 1.741l5.443 1.693L18.953 5.78c.595-.394 1.136-.176.691.218z" />
+  </svg>
+)
