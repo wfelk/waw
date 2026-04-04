e98b56f34c4ab1e2a5fea87b446f0531eb3d99a7
Wilhelm Felk <w.felk@me.com>
2026-04-05 00:31:56 +0200

    feat(contact): add telegram button

--- src/components/sections/SectionStart.tsx
+++ src/components/sections/SectionStart.tsx
@@ -59,7 +59,7 @@ export const SectionStart = () => {
 
         <div className="relative z-10 mt-auto mb-8 flex w-full flex-col items-center px-8 tablet:mb-12 tablet:flex-row tablet:justify-center tablet:gap-8">
           <div className="flex w-full max-w-fit flex-col gap-4 tablet:contents">
-            <div className="flex gap-4">
+            <div className="flex flex-wrap justify-center gap-4">
               <Button
                 href="mailto:info@waw-automobile.de"
                 icon={<Mail size={24} strokeWidth={2} />}
@@ -90,6 +90,23 @@ export const SectionStart = () => {
                 className="px-5"
               >
                 <span className="sr-only">{t("whatsapp")}</span>
+              </Button>
+              <Button
+                href="https://t.me/XXXXXXXXX"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M9.417 15.181l-.397 5.584c.568 0 .814-.244 1.109-.537l2.663-2.545 5.518 4.041c1.012.564 1.725.267 1.998-.931L23.93 3.821l.001-.001c.321-1.496-.541-2.081-1.527-1.714l-21.29 8.151c-1.453.564-1.431 1.374-.247 1.741l5.443 1.693L18.953 5.78c.595-.394 1.136-.176.691.218z" />
+                  </svg>
+                }
+                className="px-5"
+              >
+                <span className="sr-only">{t("telegram")}</span>
               </Button>
             </div>
 
